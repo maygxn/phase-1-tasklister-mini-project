7ac89c5c046288725c0bcdeef0adc61dfb854f45
Meagan <MeaganTruong1@yahoo.com>
2023-11-02 16:08:45 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const createTaskForm = document.getElementById('create-task-form');
+  const tasksElement = document.getElementById('tasks');
+
+  createTaskForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const userInput = event.target.elements['new-task-description'].value;
+    console.log(userInput)
+    
+    const listItem = document.createElement('li');
+    listItem.innerText = userInput;
+
+    tasksElement.appendChild(listItem);
+    createTaskForm.reset()
+  });
 });
